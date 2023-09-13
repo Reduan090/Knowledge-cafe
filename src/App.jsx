8ce499a11638fb4016a3395aabc87f7685f7fa32
Sajid090 <reduansazid@gmail.com>
2023-09-13 22:36:02 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Blogs from './components/header/Blogs/Blogs'
+import Header from './components/header/Header'
+
+function App() {
+
+  return (
+    <>
+      <Header></Header>
+      <Blogs></Blogs>
+    </>
+  )
+}
+
+export default App
